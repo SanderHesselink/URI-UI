db9d92430ee6bbaeadcb89067eae73574c8060bc
Sander Hesselink <s.hesselink@umail.leidenuniv.nl>
2024-08-18 14:57:05 +0200

    Linked UI to functionality

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
 from img_to_uri import image_to_uri
 import pyperclip as pc
 import tkinter as tk
+import tkinter.scrolledtext
 
 def Main():
     root = tk.Tk()
     root.title("Image to URI converter")
     root.geometry('600x600')
 
-    # REFERENCE CODE
-    # label = tk.Label(root, text="hello")
-    # label.grid()
-    # entry = tk.Entry(root, width=10)
-    # entry.grid()
-    #
-    # def click():
-    #     value = entry.get()
-    #     label.configure(text=value)
-    #
-    # button = tk.Button(root, text="Click", command=click)
-    # button.grid(column=1, row=0)
-
     tk.Label(root, text="Enter filepath:").grid(row=0)
     fileinput = tk.Entry(root, width=100)
     fileinput.grid(row=1)
 
-    output = tk.Label(root, text="")
+    output = tk.scrolledtext.ScrolledText(root, wrap=tk.WORD, width=100, height=50)
     output.grid(row=3)
 
     def convert():
         path = fileinput.get()
         if path:
-            output.configure(text=path)
+            path = path.replace('\\', '/')
+            output.insert(tk.END, image_to_uri(path))
         else:
-            output.configure(text="No input!")
+            output.insert(tk.END, "No input!")
 
     convertbutton = tk.Button(root, text="Convert", command=convert)
     convertbutton.grid(row=2)
