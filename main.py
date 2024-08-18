df1ec23b3c61c81a012691b4922c96c915ca960a
Sander Hesselink <s.hesselink@umail.leidenuniv.nl>
2024-08-18 15:17:11 +0200

    Added file explorer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from img_to_uri import image_to_uri
 import pyperclip as pc
 import tkinter as tk
-import tkinter.scrolledtext
+from tkinter import filedialog
 
 def Main():
     root = tk.Tk()
@@ -9,17 +9,25 @@ def Main():
     root.geometry('600x600')
 
     tk.Label(root, text="Enter filepath:").grid(row=0)
-    fileinput = tk.Entry(root, width=100)
+    fileinput = tk.Entry(root, width=50)
     fileinput.grid(row=1)
+
+    def dialogpopup():
+        filepath = tk.filedialog.askopenfilename()
+        fileinput.delete(0, tk.END)
+        fileinput.insert(tk.END, filepath)
+
+    fileexplorer = tk.Button(root, text="select file...", command=dialogpopup)
+    fileexplorer.grid(row=1, column=1)
 
     output = tk.Label(root, text="")
     output.grid(row=3)
 
     def convert():
-        path = fileinput.get()
-        if path:
-            path = path.replace('\\', '/')
-            pc.copy(image_to_uri(path))
+        filepath = fileinput.get()
+        if filepath:
+            filepath = filepath.replace('\\', '/')
+            pc.copy(image_to_uri(filepath))
             output.configure(text="Copied to clipboard!")
         else:
             output.configure(text="No input!")
